2673cc80335825aed03584e5bfa4c4f865dc5aeb
harvanir <harvanir@gmail.com>
2020-01-06 10:33:08 +0700

    Update code

--- src/main/java/org/harvanir/pattern/clean/CleanArchitectureApplication.java
+++ src/main/java/org/harvanir/pattern/clean/CleanArchitectureApplication.java
@@ -2,7 +2,9 @@ package org.harvanir.pattern.clean;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.retry.annotation.EnableRetry;
 
+@EnableRetry
 @SpringBootApplication
 public class CleanArchitectureApplication {
 
--- src/main/java/org/harvanir/pattern/clean/app/item/usecase/ItemIncreaseInteractorImpl.java
+++ src/main/java/org/harvanir/pattern/clean/app/item/usecase/ItemIncreaseInteractorImpl.java
@@ -3,6 +3,10 @@ package org.harvanir.pattern.clean.app.item.usecase;
 import org.harvanir.pattern.clean.app.item.entity.ItemIncreaseRequest;
 import org.harvanir.pattern.clean.app.item.entity.ItemResponse;
 import org.harvanir.pattern.clean.app.item.gateway.ItemGateway;
+import org.hibernate.StaleObjectStateException;
+import org.springframework.orm.ObjectOptimisticLockingFailureException;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Retryable;
 
 /** @author Harvan Irsyadi */
 public class ItemIncreaseInteractorImpl implements ItemIncreaseInteractor {
@@ -13,6 +17,10 @@ public class ItemIncreaseInteractorImpl implements ItemIncreaseInteractor {
     this.itemGateway = itemGateway;
   }
 
+  @Retryable(
+          value = {StaleObjectStateException.class, ObjectOptimisticLockingFailureException.class},
+          maxAttemptsExpression = "${app.retry.max-attempts}",
+          backoff = @Backoff(delayExpression = "${app.retry.delay}"))
   @Override
   public void increase(ItemIncreaseRequest request, ItemIncreasePresenter presenter) {
     ItemResponse response = itemGateway.increase(request.getId(), request.getIncrement());
