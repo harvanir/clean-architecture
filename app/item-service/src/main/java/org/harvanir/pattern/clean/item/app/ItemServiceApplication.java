84192a8cf56ec97257e3085a9e276f20da203b8c
harvanir <harvanir@gmail.com>
2020-05-12 18:35:34 +0700

    Polishing service application

---
+++ app/item-service/src/main/java/org/harvanir/pattern/clean/item/app/ItemServiceApplication.java
@@ -0,0 +1,13 @@
+package org.harvanir.pattern.clean.item.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/** @author Harvan Irsyadi */
+@SpringBootApplication
+public class ItemServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ItemServiceApplication.class, args);
+  }
+}
