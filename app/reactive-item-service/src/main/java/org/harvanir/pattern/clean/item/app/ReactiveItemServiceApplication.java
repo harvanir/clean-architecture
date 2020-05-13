7f953b3f0c1e93fc94de2d06b6e1bad7df3ec6fa
harvanir <harvanir@gmail.com>
2020-05-14 02:30:47 +0700

    Rename application class

--- app/reactive-item-service/src/main/java/org/harvanir/pattern/clean/item/app/ReactiveItemServiceApplication.java
+++ app/reactive-item-service/src/main/java/org/harvanir/pattern/clean/item/app/ReactiveItemServiceApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ReactiveCleanArchitectureApplication {
+public class ReactiveItemServiceApplication {
 
   public static void main(String[] args) {
-    SpringApplication.run(ReactiveCleanArchitectureApplication.class, args);
+    SpringApplication.run(ReactiveItemServiceApplication.class, args);
   }
 }
