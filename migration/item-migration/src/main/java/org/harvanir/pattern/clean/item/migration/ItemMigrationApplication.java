244ad10f97a03a824bc8afb32e98e8ac3e1030a0
harvanir <harvanir@gmail.com>
2020-12-05 12:45:29 +0700

    Polishing properties, improve migration module

--- migration/item-migration/src/main/java/org/harvanir/pattern/clean/item/migration/ItemMigrationApplication.java
+++ migration/item-migration/src/main/java/org/harvanir/pattern/clean/item/migration/ItemMigrationApplication.java
@@ -1,13 +1,19 @@
 package org.harvanir.pattern.clean.item.migration;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.annotation.Import;
 
 /** @author Harvan Irsyadi */
-@SpringBootApplication
+@Import({DataSourceAutoConfiguration.class, FlywayAutoConfiguration.class})
 public class ItemMigrationApplication {
 
   public static void main(String[] args) {
-    SpringApplication.run(ItemMigrationApplication.class, args);
+    new SpringApplicationBuilder(ItemMigrationApplication.class)
+        .web(WebApplicationType.NONE)
+        .build()
+        .run(args);
   }
 }
